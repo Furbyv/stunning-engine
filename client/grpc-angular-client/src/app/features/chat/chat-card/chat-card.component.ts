19d34d45d4fbccf3237f1c125c0620b7e3d425e6
Furbyv <fbverbeek@gmail.com>
2022-05-23 13:14:30 +0200

    connect send message to client

--- client/grpc-angular-client/src/app/features/chat/chat-card/chat-card.component.ts
+++ client/grpc-angular-client/src/app/features/chat/chat-card/chat-card.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Message } from 'src/app/protos/message.pb';
 import { MessageService } from '../services/message.service';
 
@@ -9,8 +9,16 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['chat-card.component.scss']
 })
 export class ChartCardComponent {
+  username: string = '';
+  message: string = '';
   messages$: Observable<Message> = this.messageService.messageStream$;
+  isConnected$ = this.messageService.connected$;
+  sending$ = this.messageService.sendMessage$.pipe(map(state => state.loading));
   constructor(private messageService: MessageService) {}
+
+  sendMessage(message: string) {
+    this.messageService.sendMessage(message);
+  }
 
   joinChat(username: string) {
     this.messageService.connectToChat(username);
--- client/grpc-angular-client/src/app/features/chat/services/message.service.ts
+++ client/grpc-angular-client/src/app/features/chat/services/message.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
+import { toAsyncState } from '@ngneat/loadoff';
 import { ReplaySubject, Subject, switchMap, withLatestFrom } from 'rxjs';
-import { Connect } from 'src/app/protos/message.pb';
+import { Connect, Message } from 'src/app/protos/message.pb';
 import { MessagingClient } from 'src/app/protos/message.pbsc';
 
 @Injectable({ providedIn: 'root' })
 export class MessageService {
   private connect$$: Subject<Connect> = new ReplaySubject<Connect>(1);
+  connected$ = this.connect$$.asObservable();
   private disconnect$$: Subject<void> = new ReplaySubject<void>(1);
+  private message$$: Subject<string> = new ReplaySubject<string>(1);
+
   messageStream$ = this.connect$$.pipe(
     switchMap(c => this.messagingClient.connectToChat(c))
   );
+
   disconnect$ = this.disconnect$$.pipe(
     withLatestFrom(this.connect$$),
     switchMap(([_, c]) => this.messagingClient.unsubscribe(c))
   );
+  sendMessage$ = this.message$$.pipe(
+    withLatestFrom(this.connect$$),
+    switchMap(([message, connection]) =>
+      this.messagingClient
+        .sendMessage(new Message({ username: connection.username, message }))
+        .pipe(toAsyncState())
+    )
+  );
 
   constructor(private messagingClient: MessagingClient) {}
+
+  sendMessage(message: string) {
+    this.message$$.next(message);
+  }
 
   connectToChat(username: string) {
     this.connect$$.next(new Connect({ username }));
